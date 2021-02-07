6116b57459a6d5b9025d498be4a48f37df369e8e
jwu0 <jwu0@users.noreply.github.com>
2021-02-07 13:06:02 -1000

    Update sumofnumbers.js

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -7,5 +7,30 @@ function sumForLoop(number) {
   }
   return sum;
 }
-
 console.log(sumForLoop(test));
+
+function sumWhileLoop(number) {
+  let i = 0;
+  let sum = 0;
+  while (i < number.length) {
+    sum += number[i];
+    i++;
+  }
+  return sum;
+}
+console.log(sumWhileLoop(test));
+
+function sumRecursion(number) {
+  if (number.length === 0) {
+    return 0;
+  }
+  return number[0] + sumRecursion(number.slice(1, number.length));
+}
+console.log(sumRecursion(test));
+
+function sumTheSimpleWay(number) {
+  return _.reduce(number, function (memo, num) {
+    return memo + num;
+  }, 0);
+}
+console.log(sumTheSimpleWay(test));
